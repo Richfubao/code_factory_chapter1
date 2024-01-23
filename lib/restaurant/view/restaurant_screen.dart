0d8f32782680762053e3744dc1592a634900654a
LeeChungWoo <merchant.fubao@gamil.com>
2024-01-24 00:19:40 +0900

    Modify: model 사용해서 응답받게 변경

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -1,4 +1,5 @@
 import 'package:code_factory/restaurant/component/restaurant_card.dart';
+import 'package:code_factory/restaurant/model/restaurant_model.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
@@ -42,21 +43,30 @@ class RestaurantScreen extends StatelessWidget {
                   itemBuilder: (_, index) {
                     final item = snapshot.data![index];
 
-                    return RestaurantCard(
-                      image: Image.network(
-                        'http://$ip${item['thumbUrl']}',
-                        fit: BoxFit.cover,
-                      ),
-                      /*   image: Image.asset(
-                        'asset/img/food/ddeok_bok_gi.jpg',
-                        fit: BoxFit.cover,
-                      ),*/
+                    final pItem = RestaurantModel(
+                      id: item['id'],
                       name: item['name'],
+                      thumbUrl: 'http://$ip${item['thumbUrl']}',
                       tags: List<String>.from(item['tags']),
+                      priceRange: RestaurantPriceRange.values
+                          .firstWhere((e) => e.name == item['priceRange']),
+                      ratings: item['ratings'],
                       ratingsCount: item['ratingsCount'],
                       deliveryTime: item['deliveryTime'],
                       deliveryFee: item['deliveryFee'],
-                      ratings: item['ratings'],
+                    );
+
+                    return RestaurantCard(
+                      image: Image.network(
+                        pItem.thumbUrl,
+                        fit: BoxFit.cover,
+                      ),
+                      name: pItem.name,
+                      tags: pItem.tags,
+                      ratingsCount: pItem.ratingsCount,
+                      deliveryTime: pItem.deliveryTime,
+                      deliveryFee: pItem.deliveryFee,
+                      ratings: pItem.ratings,
                     );
                   },
                   separatorBuilder: (_, index) {

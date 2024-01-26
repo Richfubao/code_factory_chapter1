2419409f5da78e9a747caff1edd152992668add1
fubao <merchant.fubao@gamil.com>
2024-01-26 12:13:44 +0900

    Add: product card 사용

--- lib/restaurant/view/restaurant_detail_screen.dart
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:code_factory/common/layout/default_layout.dart';
+import 'package:code_factory/product/component/product_card.dart';
 import 'package:code_factory/restaurant/component/restaurant_card.dart';
 import 'package:flutter/material.dart';
 
@@ -21,6 +22,10 @@ class RestaurantDetailScreen extends StatelessWidget {
             ratings: 4.76,
             isDetail: true,
             detail: '맛있는 떡볶이',
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: ProductCard(),
           ),
         ],
       ),
